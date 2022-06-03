c3a5d606157b95352d67c5bed198ff315aa20fec
zunigamario <74891797+zunigamario@users.noreply.github.com>
2022-06-03 15:16:59 -0700

    updated experience.html

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("ext.html", title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/about')
+def about():
+    return render_template('about.html', url=os.getenv("URL"))
+
+@app.route('/experience')
+def experience():
+    return render_template('experience.html', url=os.getenv("URL"))
+
+@app.route('/education')
+def education():
+    return render_template('education.html', url=os.getenv("URL"))
+
+@app.route('/hobbies')
+def hobbies():
+    return render_template('hobbies.html', url=os.getenv("URL"))
+
