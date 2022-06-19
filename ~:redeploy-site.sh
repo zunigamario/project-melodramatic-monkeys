74f410b8ba0bf3e6ebba89f23f897c6ccf286318
zunigamario <zunigamario01@gmail.com>
2022-06-19 16:27:31 -0700

    Update ~:redeploy-site.sh

--- ~:redeploy-site.sh
+++ ~:redeploy-site.sh
@@ -4,7 +4,6 @@ for file in $(tmux ls)
 do
     tmux kill-session -t "$file"
 done
-cd project-melodramatic-monkeys
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
