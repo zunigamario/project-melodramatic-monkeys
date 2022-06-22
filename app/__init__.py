2973df4e64374c82ea28be3483c71f2a24c0c4c9
zunigamario <zunigamario01@gmail.com>
2022-06-22 13:54:07 -0700

    redid part 1 of adding databases

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,14 @@ from playhouse.shortcuts import model_to_dict
 load_dotenv("example.env")
 app = Flask(__name__)
 
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306
+)
+
+print(mydb)
 
 
 
