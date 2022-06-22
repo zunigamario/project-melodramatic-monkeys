76938d95be4565db814c7c22ab05789791612369
zunigamario <zunigamario01@gmail.com>
2022-06-22 15:27:54 -0700

    updates

--- app/__init__.py
+++ app/__init__.py
@@ -81,8 +81,8 @@ class TimelinePost(Model):
     class Meta:
         database = mydb
 
-# mydb.connect()
-# mydb.create_tables([TimelinePost])
+mydb.connect()
+mydb.create_tables([TimelinePost])
 
 @app.route('/api/timeline_post', methods=['POST'])
 def post_time_line_post():
