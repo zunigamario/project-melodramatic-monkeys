70d74ee48bff41c1436c16c7d21f8403cad6572e
zunigamario <zunigamario01@gmail.com>
2022-06-23 20:34:49 -0700

    testing redeploy changes

--- ~:redeploy-site.sh
+++ ~:redeploy-site.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-for file in $(tmux ls)
-do
-    tmux kill-session -t "$file"
-done
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-
-tmux new -d 'flask run --host=0.0.0.0'
+systemctl restart myportfolio
